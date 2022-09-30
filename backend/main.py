323fbf1d151f4b1fe7fa8b34a05ef5f1e682856b
hzco <73980202+hzco@users.noreply.github.com>
2022-09-30 15:49:48 -0400

    Added API endpoint for yfinance python library

---
+++ backend/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI, Response
+import yfinance
+
+app = FastAPI()
+
+@app.get("/stock-data")
+def get_stock_data(tickerSymbol):
+	#data will be downloaded as a pandas.dataframe
+	data = yfinance.download(tickerSymbol)
+	#changing date column from index to regualar column
+	data.reset_index(inplace=True)
+	return Response(data.to_json(orient="records"), media_type="application/json")
