12df7aac3e2242309c1b1f8ac96a8863fba2a7d6
hzco <73980202+hzco@users.noreply.github.com>
2022-09-30 16:09:25 -0400

    added another comment

--- backend/main.py
+++ backend/main.py
@@ -9,4 +9,4 @@ def get_stock_data(tickerSymbol):
 	data = yfinance.download(tickerSymbol)
 	#changing date column from index to regualar column
 	data.reset_index(inplace=True)
-	return Response(data.to_json(orient="records"), media_type="application/json")
+	return Response(data.to_json(orient="records"), media_type="application/json") #formats into json format before returning
