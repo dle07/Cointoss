0a7f0a36bee36e5d111baa03f8d78fcdd392b376
Daniel Le <daniel15963le@gmail.com>
2022-10-04 18:46:05 -0400

    reddit twitter scrapper done

--- src/utils/ConfigUtils.py
+++ src/utils/ConfigUtils.py
@@ -8,8 +8,15 @@ class ConfigUtils():
     with open(path_to_config, 'r') as f:
         valuesYaml = yaml.load(f, Loader=yaml.FullLoader)
 
-    API_KEY = valuesYaml["twitter"]["api_key"]
-    API_KEY_SECRET = valuesYaml["twitter"]["api_key_secret"]
+    TWITTER_API_KEY = valuesYaml["twitter"]["api_key"]
+    TWITTER_API_KEY_SECRET = valuesYaml["twitter"]["api_key_secret"]
     BEARER_TOKEN = valuesYaml["twitter"]["bearer_token"]
+
+    REDDIT_CLIENT_ID = valuesYaml["reddit"]['client_id']
+    REDDIT_CLIENT_SECRET = valuesYaml['reddit']['secret']
+
+
+
+
 
 
--- src/utils/web_scrapper.py
+++ src/utils/web_scrapper.py
@@ -1,3 +1,5 @@
+from datetime import datetime, timedelta
+from pathlib import Path
 from pprint import pprint
 import tweepy
 from src.utils.ConfigUtils import ConfigUtils
@@ -5,6 +7,7 @@ import re
 import csv
 import requests
 from src.utils.storage_utils import create_csv_path
+import praw
 
 """
 Querying
@@ -12,43 +15,66 @@ https://github.com/twitterdev/getting-started-with-the-twitter-api-v2-for-academ
 """
 
 
-API_KEY = ConfigUtils.API_KEY
-API_KEY_SECRET = ConfigUtils.API_KEY_SECRET
-BEARER_TOKEN = ConfigUtils.BEARER_TOKEN
+TWITTER_API_KEY = ConfigUtils.TWITTER_API_KEY
+TWITTER_API_KEY_SECRET = ConfigUtils.TWITTER_API_KEY_SECRET
+TWITTER_BEARER_TOKEN = ConfigUtils.BEARER_TOKEN
+
+REDDIT_CLIENT_ID = ConfigUtils.REDDIT_CLIENT_ID
+REDDIT_CLIENT_SECRET = ConfigUtils.REDDIT_CLIENT_SECRET
+
+def scrapeData(ticker) -> Path:
+
+    file_path = create_csv_path("sentiment")
+    twitter_data = queryByTickerTwitter(ticker)
+    reddit_data = queryByTickerReddit(ticker)
+
+    with open(file_path ,mode = 'a', newline='', encoding='utf-8') as csvFile:
+        writer = csv.writer(csvFile, delimiter=',')
+        writer.writerow(["text,created_at"])
+        writer.writerows(twitter_data)
+        writer.writerows(reddit_data)
+    return Path(file_path)
+        
 
 
-
-class Tweepy():
-
-    pass
 
 def queryByTickerTwitter(ticker:str):
-    client = tweepy.Client(bearer_token = BEARER_TOKEN)
+    client = tweepy.Client(bearer_token = TWITTER_BEARER_TOKEN)
     query = ticker.upper().lstrip('$') + " lang:en -is:retweet"
-    filtered = []
-    cols = ["text,created_at"]
-    file_name = create_csv_path("twitter")
+    rows = []
+    
+    for tweet in tweepy.Paginator(client.search_recent_tweets, query=query,
+                            tweet_fields=['created_at'], max_results=100).flatten(limit=1000):
 
-    with open(file_name,mode = 'a', newline='', encoding='utf-8') as csvFile:
-        writer = csv.writer(csvFile, delimiter=',')
-        writer.writerow(cols)
-        for tweet in tweepy.Paginator(client.search_recent_tweets, query=query,
-                                tweet_fields=['created_at'], max_results=100).flatten(limit=1000):
-
-            if( re.search('\$VOO', tweet.text) != None):
-                row = [tweet.text]
-                filtered.append(tweet)
-                if(tweet.created_at != None):
-                    row.append(tweet.created_at)
-                writer.writerow(row)
-
-def queryByTickerReddit(ticker:str):
-    pass
+        if( re.search('\$VOO', tweet.text) != None):
+            row = [tweet.text, str(tweet.created_at)]
+            rows.append(row)
+    return rows
 
 
-def testRedditAPI():
 
-    pass
+
+"""
+PRAW RATE LIMITS
+
+"""
+def queryByTickerReddit(ticker:str, limit = 1000):
+    # subreddits to query from: /r/stocks /r/wallstreetbets /r/investing /r/StockMarket
+    now = datetime.now()
+    reddit = praw.Reddit( client_id=REDDIT_CLIENT_ID, client_secret=REDDIT_CLIENT_SECRET, user_agent="cointossgang")
+    rows = []
+    for post in reddit.subreddit("stocks+wallstreetbets+investing+StockMarket").search(query = ticker, time_filter="week",limit = limit ,sort = "relevance"):
+        created = datetime.fromtimestamp(post.created_utc)
+        if (now - created).days <=3 :  # Check to see if post is within 3 days
+            rows.append([post.selftext, str(created)])
+    return rows
+
+    """
+    post.selftext: text of post
+
+    """
+
+
                     
             
 
