7e4d3860034ca82a1184dd2290934555d1b60fc1
jasonwang70 <75920454+jasonwang70@users.noreply.github.com>
2022-12-05 19:58:03 -0500

    some styling/prediction

--- frontend/src/pages/portfolio.js
+++ frontend/src/pages/portfolio.js
@@ -3,11 +3,16 @@ import {SearchBar} from '../components/SearchBar';
 import {useCookies} from 'react-cookie';
 import axios from 'axios';
 import CloseIcon from '@mui/icons-material/Close';
+import { useNavigate } from 'react-router-dom';
 
 export default function Portfolio() {
   const [cookies] = useCookies(['jwt']);
   const [tickerList, setTickerList] = useState([]);
   const [tickerPrice, setTickerPrice] = useState([]);
+  const [nextDayPred, setNextDayPred] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const promises = [];
+  const navigate = useNavigate();
   
   const deleteTicker = async(ticker) => {
     const token = `Bearer ${cookies.jwt.auth_token}`;
@@ -40,7 +45,22 @@ export default function Portfolio() {
         },
       }).then(res => res.json()).then((ticker) => {
         setTickerList(ticker.tracked_tickers);
-        axios.get(`/stock-data?tickerSymbol=${ticker.tracked_tickers}&timePeriod=1d`).then(res => setTickerPrice(res.data[0])) 
+        if(ticker.tracked_tickers.length > 1) {
+          axios.get(`/stock-data?tickerSymbol=${ticker.tracked_tickers}&timePeriod=1d`).then(res => setTickerPrice(res.data[0])) 
+        } else if (ticker.tracked_tickers.length === 1) {
+          axios.get(`/stock-data?tickerSymbol=${ticker.tracked_tickers}`).then(res => setTickerPrice(res.data));
+        }
+        for(let i = 0; i < ticker.tracked_tickers.length; i++) {
+          promises.push(axios.get(`/ml/time-series?tickerSymbol=${ticker.tracked_tickers[i]}`));
+        }
+
+        Promise.all(promises).then(responses => {
+          for(let j = 0; j < ticker.tracked_tickers.length; j++) {
+            let nextDayprediction = responses[j].data[0].prediction;
+            setNextDayPred(prediction => [...prediction, nextDayprediction]);
+            setLoading(false);
+          }
+        });  
       });
     } catch (err) {
       console.log(err);
@@ -66,6 +86,7 @@ export default function Portfolio() {
           <tr>
             <th scope='col'>Ticker Symbol</th>
             <th scope='col'>Price</th>
+            <th scope='col'>Prediciton(Next Day)</th>
             <th scope='col'></th>
           </tr>
         </thead>
@@ -73,8 +94,11 @@ export default function Portfolio() {
           {tickerList.map((eachTicker, i) => {
             return (
               <tr key={i}>
-                <td>{eachTicker}</td>
-                <td>{tickerPrice[`('Close', '${eachTicker}')`]?.toFixed(2)}</td>
+                <td onClick={() => {navigate(`/StockPrice/${eachTicker}`)}}>{eachTicker}</td>
+                {(tickerList.length === 1) ? <td>{tickerPrice[tickerPrice.length - 1]?.Close.toFixed(2)}</td> : 
+                <td>{tickerPrice[`('Close', '${eachTicker}')`]?.toFixed(2)}</td>}
+                {(loading) ? <td>Loading...</td> : 
+                <td>{nextDayPred[i]?.toFixed(2)}</td>}
                 <td onClick={() => {deleteTicker(eachTicker)}} style={{color: 'red'}}><CloseIcon/></td>
               </tr>
             );
