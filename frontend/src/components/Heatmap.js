50ffe9f95b0f9b2655064e6e502b6db48d5f31ea
jasonwang70 <75920454+jasonwang70@users.noreply.github.com>
2022-12-12 21:53:02 -0500

    updated heatmap, searchbar

--- frontend/src/components/Heatmap.js
+++ frontend/src/components/Heatmap.js
@@ -18,21 +18,11 @@ let dataPosition = new Array(yLabels.length)
     .map(() =>
         new Array(xLabels.length).fill(0));
 
-const trendTickersVolume = new Array(yLabels.length)
-    .fill(0)
-    .map(() =>
-        new Array(xLabels.length).fill(0));
-
 function Heatmap() {
     const [loading, setLoading] = useState(true);
-    //const [rawData, setRawData] = useState([]);
     const [trendingTickers, setTrendingTickers] = useState([]);
-    //const [modelPredictions, setModelPredictions] = useState([]);
     const promises = [];
     const [priceDiffArray, setPriceDiffArray] = useState([]);
-    //const [priceDiff, setPriceDiff] = useState([]);
-    const arr = [];
-    //grabs data from backend (top 21 tickers by volume and their prices)
     const fetchData = async () => {
         axios.get(`/highest-volume?limit=21`).then(res => {
             setTrendingTickers(res.data.tickers);
@@ -41,20 +31,14 @@ function Heatmap() {
                 for(let i = 0; i < res.data.tickers.length; i++) {
                     promises.push(axios.get(`/ml/time-series?tickerSymbol=${res.data.tickers[i]}`));
                 }
-                //axios.get(`/ml/time-series?tickerSymbol=AAPL`).then(res => setModelPredictions(res.data));
                 Promise.all(promises).then(responses => {
-                    //console.log(typeof(responses[0].data[4].prediction))
-                    //console.log(typeof(price.data[0][`('Close', '${res.data.tickers[0]}')`]))
-                    //console.log(responses[0].data[4].prediction - price.data[0][`('Close', '${res.data.tickers[0]}')`])
                     let tickCounter = 0;
                     for (let j = 0; j < data.length; ++j) {
                         for (let k = 0; k < data[j].length; ++k) {
                             let difference_in_price = responses[tickCounter].data[4].prediction - price.data[0][`('Close', '${res.data.tickers[tickCounter]}')`];
-                            //setPriceDiff(difference_in_price);
                             setPriceDiffArray(priceDiff => [...priceDiff, difference_in_price]);
-                            data[j][k] = res.data.tickers[tickCounter] + '\n' + price.data[0][`('Close', '${res.data.tickers[tickCounter]}')`]?.toFixed(2) + '\n/ Pre: ' 
+                            data[j][k] = res.data.tickers[tickCounter] + '\n' + price.data[0][`('Close', '${res.data.tickers[tickCounter]}')`]?.toFixed(2) + '\n/ Pred: ' 
                             + responses[tickCounter].data[4].prediction?.toFixed(2);
-                            //trendTickersVolume[j][k] = rawData[`('Volume', '${trendingTickers[tickCounter]}')`];
                             ++tickCounter;
                         }
                     }
@@ -66,12 +50,6 @@ function Heatmap() {
     useEffect(() => {
         fetchData();
     }, []);
-
-    //console.log(modelPredictions);
-    
-    
-    //console.log(trendTickersVolume);
-    //console.log(Math.max(trendTickersVolume))
 
     //stores tickers in 2d array format
     let tickCount = 0;
@@ -87,20 +65,24 @@ function Heatmap() {
     }
 
     let count = 0;
-    console.log(priceDiffArray);
-    for(let i = 0; i < priceDiffArray.length; i++) {
-        if(priceDiffArray[i] < 0) {
-            arr.push(0);
+
+    const color_change = () => {
+        let index = count - 1;
+        if(priceDiffArray[index] < 0) {
+            return (Math.min(...priceDiffArray) - (Math.min(...priceDiffArray) - (priceDiffArray[index]))) / Math.min(...priceDiffArray) + 0.1;
         } else {
-            arr.push(1);
+            if(Math.max(...priceDiffArray) - priceDiffArray[index] === 0) {
+                return 1;
+            } else {
+                return (Math.max(...priceDiffArray) - (Math.max(...priceDiffArray) - priceDiffArray[index])) / Math.max(...priceDiffArray) + 0.1;
+            }
         }
     }
-    console.log(arr);
 
     return (
         <>
             <div style={{margin: "0px", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                <div style={{background: "red", paddingRight: "40px"}}>
+                <div style={{background: "lightgray", paddingRight: "40px"}}>
                     <div style={{marginLeft: "-39px"}}>
                         <HeatMap
                             xLabels={xLabels}
@@ -112,9 +94,11 @@ function Heatmap() {
                             squares
                             onClick={(x, y) => navigate(`/StockPrice/${dataPosition[y][x]}`)}
                             cellStyle={(background, value, min, max, data, x, y, color, fontWeight) => ({ 
-                                background: `rgba(17, 255, 00, ${arr[count++]/1})`,
+                                background: (priceDiffArray[count++] < 0) ? `rgba(255,0,0,${color_change()})` : 
+                                `rgba(0,255,0,${color_change()})`,
                                 fontSize: "20px",
-                                fontWeight: "bold"
+                                fontWeight: "bold",
+                                border: "2px solid black"
                             })}
                             cellRender={(value) => value}
                             title={(value, unit) => `${value}`}
